b88fc1d10ae33be3e0e201ddb7b3f1ac9e72a1c3
Chaser1oo <c047mp55@gmail.com>
2022-02-13 13:08:23 +0300

    Git

---
+++ src/Java1lesson1/L1N1.java
@@ -0,0 +1,9 @@
+package Java1lesson1;
+
+public class L1N1 {
+    public static void main (String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
