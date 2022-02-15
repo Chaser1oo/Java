54f07683b88611e312837e3ee503bce47083085d
Chaser1oo <c047mp55@gmail.com>
2022-02-15 21:15:30 +0300

    GE

--- src/Java1Lesson3/L3N5.java
+++ src/Java1Lesson3/L3N5.java
@@ -1,4 +1,16 @@
 package Java1Lesson3;
 
+import java.util.Arrays;
+
 public class L3N5 {
+    public static void main(String args[]) {
+        int initialValue = 5;
+        int len = 3;
+
+        int[] nums = new int[len];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = initialValue;
+        }
+        System.out.print(Arrays.toString(nums));
+    }
 }
--- src/Java1Lesson3/L3N6.java
+++ src/Java1Lesson3/L3N6.java
@@ -1,4 +1,20 @@
 package Java1Lesson3;
 
+import java.util.Arrays;
+
 public class L3N6 {
+
+
+    public static void main(String args[]) {
+        //int[] nums = new int[100];
+        int[] nums = { 50, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+    System.out.println(Arrays.toString(nums));
+        System.out.println(max);
+    }
 }
